a9918784def91ed4330541c8525600f53f7ad956
FedericaC03 <fdrk03@hotmail.it>
2020-08-27 15:13:05 +0200

    Fine esercizio parola palindroma

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert();
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+function palindrome(str) {
+  return str == str.split('').reverse().join('');
+}
+var parola = prompt("Inserisci una parola per verificare se è palindroma")
+console.log(palindrome(parola));
+
+
+if (palindrome(parola) == true) {
+  alert("Si " + parola +  " è una parola palindroma");
+} else {
+  alert("No,  " + parola + " non è una parola palindroma");
+}
