a880e6d7ad34e7491d98fd1baa70872540306d47
FedericaC03 <fdrk03@hotmail.it>
2020-08-27 16:55:37 +0200

    commenti aggiunti

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -11,15 +11,14 @@ function NumeroCasuale(min, max) {
 
 
 //SOMMO IL NUMERO INSERITO DALL'UTENTE CON IL NUMERO GENERATO DAL PC
-
 function SommaNumeri(num1, num2) {
   var risultato = num1 + num2;
   return risultato;
 }
-
 var somma = SommaNumeri(NumeroCasuale(1, 6), numero);
 console.log(somma);
 
+//SE L'UTENTE HA DIGITATO PARI E LA SOMMA DEI DUE NUMERI E' PARI, ALLORA L'UTENTE HA VINTO, ALTRIMENTI HA PERSO
 if ((somma % 2 == 0 && pariDispari == "Pari") || (somma % 2 == 1 && pariDispari == "Dispari")) {
   alert("Complimenti hai vinto!");
 } else if ((somma % 2 == 1 && pariDispari == "Pari") || (somma % 2 == 0 && pariDispari == "Dispari")) {
