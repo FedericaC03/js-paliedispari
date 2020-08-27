aae6dd9eb26c169910d9f7ae1405b34cbcb90e49
FedericaC03 <fdrk03@hotmail.it>
2020-08-27 16:39:59 +0200

    Esercizio completato

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -1,10 +1,32 @@
 //CHIEDO ALL'UTENTE DI INSERIRE PARI O DISPARI E POI UN NUMERO DA 1 A 5 ù
-// var pariDispari = prompt('Inserire "Pari" o "Dispari"');
-// var numero = prompt("Inserire un numero da 1 a 5");
+var pariDispari = prompt('Inserire "Pari" o "Dispari"');
+var numero = parseInt(prompt("Inserire un numero da 1 a 5"));
 
 //GENERO UN NUMERO CASUALE PER IL PC DA 1 A 5 USANDO UNA FUNZIONE
 
 function NumeroCasuale(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
-    console.log(NumeroCasuale(1,6));
+    // console.log(NumeroCasuale(1,6));
+
+
+//SOMMO IL NUMERO INSERITO DALL'UTENTE CON IL NUMERO GENERATO DAL PC
+
+function SommaNumeri(num1, num2) {
+  var risultato = num1 + num2;
+  return risultato;
+}
+
+var somma = SommaNumeri(NumeroCasuale(1, 6), numero);
+console.log(somma);
+
+if ((somma % 2 == 0 && pariDispari == "Pari") || (somma % 2 == 1 && pariDispari == "Dispari")) {
+  alert("Complimenti hai vinto!");
+} else if ((somma % 2 == 1 && pariDispari == "Pari") || (somma % 2 == 0 && pariDispari == "Dispari")) {
+  alert("Ritenta sarai più fortunato");
+}
+
+// while (pariDispari == null && numero == null) {
+//   var pariDispari = prompt('Inserire "Pari" o "Dispari"');
+//   var numero = parseInt(prompt("Inserire un numero da 1 a 5"));
+// }
